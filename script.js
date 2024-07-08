5c0ae14ad6cba64fefb9e24b65166b6343515f96
JohnyMax42 <johnyernesto1997@gmail.com>
2024-07-08 16:42:02 -0300

    jokenpo inicio

---
+++ script.js
@@ -0,0 +1,5 @@
+const opcoes = {
+   1: {nome: 'pedra', img:'img/pedra.png', pedra: 'EMPATE', papel: 'PERDEU', tesoura: 'GANHOU'},
+   2: {nome: 'papel', img:'img/papel.png', pedra: 'GANHOU', papel: 'EMPATE', tesoura: 'PERDEU'},
+   3: {nome: 'tesoura', img:'img/tesoura.png', pedra: 'PERDEU', papel: 'GANHOU', tesoura: 'EMPATE'}
+}
